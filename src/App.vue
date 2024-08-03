6dafec9d5b333e04b02d0b876ad0779dc49e4e23
david <davidcol123dm@gmail.com>
2024-08-03 01:20:26 -0500

    componetizar Guitarra

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, reactive, onMounted } from "vue";
 import { db } from "./data/guitarras.js";
+import  Guitarra  from "./components/Guitarra.vue";
 
 // const state=reactive({
 //   guitarras: []
@@ -15,7 +16,10 @@ onMounted(() => {
   console.log(guitarras.value);
 })
 
-
+const agregarCarrito = (guitarra) => {
+  
+  
+}
 
 </script>
 
@@ -97,19 +101,12 @@ onMounted(() => {
     <h2 class="text-center">Nuestra Colección</h2>
 
     <div class="row mt-5">
-      <div v-for="guitarra in guitarras" class="col-md-6 col-lg-4 my-4 row align-items-center">
-        <div class="col-4">
-          <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
-        </div>
-        <div class="col-8">
-          <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt,
-            amet deserunt</p>
-          <p class="fw-black text-primary fs-3">$299</p>
-          <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
-        </div>
-      </div><!-- FIN GUITARRA -->
-    </div><!-- FIN GUITARRA -->
+      <Guitarra
+        v-for="guitarra in guitarras"
+        v-bind:guitarra="guitarra"
+        @agregar-carrito="agregarCarrito"
+      />
+    </div>
   </main>
 
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref } from 'vue'
 
 defineProps({
@@ -40,4 +40,4 @@ const count = ref(0)
 .read-the-docs {
   color: #888;
 }
-</style>
+</style> -->
