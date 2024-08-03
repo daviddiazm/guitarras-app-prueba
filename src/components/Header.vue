d9a051f67488f3f130f430419c230695ffe40182
david <davidcol123dm@gmail.com>
2024-08-03 12:30:00 -0500

    terminado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,123 @@
 <script setup>
+import { ref, computed } from 'vue';
+import { onMounted } from 'vue';
+
+  const props = defineProps({
+    carrito:{
+      type: Array,
+      required: true
+    },
+    guitarraHome: {
+      type: Object,
+      required: true
+    }
+  })
+
+  defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito', 'eliminar-carrito', 'limpiar-carrito'])
+  
+  const totalPagar = computed(()=> {
+    return props.carrito.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0)
+  })
+
 
 </script>
 
 <template>
-  <div>
+  <header class="py-5 header">
+    <div class="container-xl">
+      <div class="row justify-content-center justify-content-md-between">
+        <div class="col-8 col-md-3">
+          <a href="index.html">
+            <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
+          </a>
+        </div>
+        <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+          <!-- <Carrito -->
+            <div class="carrito">
+            <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
-  </div>
+            <div id="carrito" class="bg-white p-3">
+              <p v-if="carrito.length === 0" class="text-center">El carrito esta vacio</p>
+              <div v-else >
+                <table 
+                  v-for="producto in carrito"
+                  class="w-100 table"
+                  >
+                  <thead>
+                    <tr>
+                      <th>Imagen</th>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <img class="img-fluid" :src="`/img/${producto.imagen}.jpg`" alt="imagen guitarra">
+                      </td>
+                      <td>{{producto.nombre}}</td>
+                      <td class="fw-bold">
+                        ${{producto.precio}}
+                      </td>
+                      <td class="flex align-items-start gap-4">
+                        <button 
+                            type="button"
+                            class="btn btn-dark"
+                            @click="$emit('decrementar-cantidad', producto.id)" >
+                          -
+                        </button>
+                        {{ producto.cantidad }}
+                        <button 
+                          type="button" 
+                          class="btn btn-dark"
+                          @click="$emit('incrementar-cantidad', producto.id)">
+                          +
+                        </button>
+                      </td>
+                      <td>
+                        <button 
+                          class="btn btn-danger" 
+                          type="button"
+                          @click="$emit('eliminar-carrito', producto.id)">
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+  
+                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
+                <button 
+                  class="btn btn-dark w-100 mt-3 p-2"
+                  @click="$emit('limpiar-carrito')">
+                  Vaciar Carrito</button>
+              </div>
+            </div>
+          </div>
+          />
+        </nav>
+      </div><!--.row-->
+
+      <div class="row mt-5">
+        <div class="col-md-6 text-center text-md-start pt-5">
+          <h1 class="display-2 fw-bold">{{ guitarraHome.nombre }}</h1>
+          <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus
+            quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio
+            exercitationem eos inventore odit.</p>
+          <p class="text-primary fs-1 fw-black">${{ guitarraHome.precio }}</p>
+          <button 
+            type="button" 
+            class="btn fs-4 bg-primary text-white py-2 px-5"
+            @click="$emit('agregar-carrito',guitarraHome)">
+            Agregar al Carrito</button>
+        </div>
+      </div>
+    </div>
+
+    <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
